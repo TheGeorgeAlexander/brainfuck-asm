52cfec5d843cdf2684eab4b5e4ee8299a8e94410
TheGeorgeAlexander <159033879+TheGeorgeAlexander@users.noreply.github.com>
2025-10-07 21:34:27 +0200

    Read code from stdin instead of embedded

--- README.md
+++ README.md
@@ -3,25 +3,22 @@
 This is a [Brainfuck](https://en.wikipedia.org/wiki/Brainfuck) interpreter written in x86_64 assembly for Linux. I made this to learn assembly, so this is my first assembly project. I chose to make this because it wasn't too big of a project, while still not as trivial as a simple "Hello World!".
 
 
-## Setting the Brainfuck code
-Open `src/interpreter.asm` and put your Brainfuck code between the quotation marks on the second line:
-```asm
-section .data
-code db         "<BRAINFUCK CODE HERE>", 0
-code_len equ    $ - code - 1
-newline db      10
-...
-```
+## Compiling
 
+> [!NOTE]
+> You will get multiple `warning: 32-bit absolute section-crossing relocation [-w+reloc-abs-dword]` when compiling. This is harmless and I understand the issue, but I'm not sure how to get rid of it without disabling warnings or creating other warnings.
 
-## Compiling and running
-
-- Requirements:
+- Requirements
   - `nasm` for compiling
   - `ld` for linking
 - Execute `make` to build to the project.
-- Execute `./interpreter` to run the Brainfuck code and see its output!
 
-> [!NOTE]
-> You will get `warning: 32-bit absolute section-crossing relocation [-w+reloc-abs-dword]` when compiling. This is harmless and I understand the issue, but I'm not sure how to get rid of it without creating other warnings.
 
+## Running
+
+> [!TIP]
+> Don't want to manually paste/type your program each time? Run it from a file with `cat path/to/your/file.b | ./interpreter`
+
+- Execute `./interpreter` to run the program
+- Enter your Brainfuck code and press enter
+- It runs! Admire the output of your beautiful code
--- src/interpreter.asm
+++ src/interpreter.asm
@@ -1,140 +1,179 @@
 section .data
-code db         "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", 0
-code_len equ    $ - code - 1
-newline db      10, 10
+ask_input db            "Enter your Brainfuck program: "
+ask_inputlen equ        $ - ask_input
+error_too_large db      10, "Error: Input too large, code can't be more than 15 kB", 10
+error_too_largelen equ  $ - error_too_large
+newlines db             10, 10
+
 
 section .bss
+codebuffer resb 15001
 buffer resb 30000
+
 
 section .text
     global _start
 
+; rax: Used for syscalls
+; rbx: Bracket counter to find correct matching bracket when jumping
+; rcx: Not used (clobbered after syscalls)
+; rdx: Used for syscalls
+; rdi: Used for syscalls
+; rsi: Used for syscalls
+; rbp: Not used
+; rsp: Not used
+; r8:  Length of code buffer
+; r9:  Brainfuck instruction pointer (starts at code buffer[0])
+; r10: Brainfuck data pointer
 _start:
-    ; Print entire brainfuck code
-    mov     rax, 1          ; syscall write
-    mov     rdi, 1          ; stdout
-    lea     rsi, [code]     ; code address
-    mov     rdx, code_len   ; code length
+    ; Ask for input
+    mov     rax, 1              ; syscall write
+    mov     rdi, 1              ; stdout
+    lea     rsi, [ask_input]    ; msg address
+    mov     rdx, ask_inputlen   ; msg length
     syscall
+
+    ; Read Brainfuck code from stdin
+    mov     rax, 0              ; syscall read
+    mov     rdi, 0              ; stdin
+    lea     rsi, [codebuffer]   ; storage address
+    mov     rdx, 15001          ; Read max of 15001 bytes
+    syscall
+
+    cmp     rax, 15000                  ; Error if output is more than 15,000 bytes
+    jle     .good_input
+    mov     rax, 1                      ; syscall write
+    mov     rdi, 2                      ; stderr
+    lea     rsi, [error_too_large]      ; error msg address
+    mov     rdx, error_too_largelen     ; error msg length
+    syscall
+    mov     rax, 60                     ; syscall exit
+    mov     rdi, 1                      ; exit code 1
+    syscall
+
+.good_input:
+    mov     r8, rax             ; Length of input
 
     ; Print double newline
     mov     rax, 1          ; syscall write
     mov     rdi, 1          ; stdout
-    lea     rsi, [newline]  ; newline character address
+    lea     rsi, [newlines] ; newline characters address
     mov     rdx, 2          ; 2 characters
     syscall
 
-    ; Store address to the code in rbx
-    lea     rbx, [code - 1]
-
-    ; Brainfuck data pointer
-    lea     r12, [buffer]
+    lea     r9, [codebuffer - 1]    ; Brainfuck instruction pointer
+    lea     r10, [buffer]           ; Brainfuck data pointer
 
 ; Loop over all characters in the code
 char_loop:
-    inc     rbx
-    mov     al, [rbx]
-    cmp     al, 0
+    inc     r9         ; Increment instruction pointer
+    mov     al, [r9]   ; Load instruction
+
+    test    al, al      ; NULL means end of program, so exit
     je      exit
 
+    ; See if character matches a Brainfuck operation
     cmp     al, '>'
-    je      datapointer_right
+    je      op_right
     cmp     al, '<'
-    je      datapointer_left
+    je      op_left
     cmp     al, '+'
-    je      inc_value
+    je      op_inc
     cmp     al, '-'
-    je      dec_value
+    je      op_dec
     cmp     al, '.'
-    je      output_value
+    je      op_output
     cmp     al, ','
-    je      input_value
+    je      op_input
     cmp     al, '['
-    je      if_zero_go_forward
+    je      op_jmp_fw
     cmp     al, ']'
-    je      if_nonzero_go_back
+    je      op_jmp_bw
+
+    jmp char_loop       ; Unknown character, just continue to next one
 
 
-datapointer_right:
-    inc     r12
+op_right:
+    inc     r10
     jmp     char_loop
 
-datapointer_left:
-    dec     r12
+
+op_left:
+    dec     r10
     jmp     char_loop
 
-inc_value:
-    inc     byte [r12]
+
+op_inc:
+    inc     byte [r10]
     jmp     char_loop
 
-dec_value:
-    dec     byte [r12]
+
+op_dec:
+    dec     byte [r10]
     jmp     char_loop
 
-output_value:
+
+op_output:
     mov     rax, 1      ; syscall write
     mov     rdi, 1      ; stdout
-    lea     rsi, [r12]  ; buffer address
+    lea     rsi, [r10]  ; buffer address
     mov     rdx, 1      ; 1 character
     syscall
     jmp     char_loop
 
-input_value:
+
+op_input:
     mov     rax, 0          ; syscall read
     mov     rdi, 0          ; stdin
-    lea     rsi, [r12]      ; buffer address
+    lea     rsi, [r10]      ; buffer address
     mov     rdx, 1          ; 1 character
     syscall
-    cmp     rax, 0
+    cmp     rax, 0          ; 0 characters read (reached EOF)? Set byte to -1
     jne     char_loop
-    mov     byte [r12], -1  ; Set byte to -1 if EOF
+    mov     byte [r10], -1
     jmp     char_loop
 
-if_zero_go_forward:
-    ; Don't do anything if data pointer is not 0
-    cmp     byte [r12], 0
+
+op_jmp_fw:
+    cmp     byte [r10], 0           ; Don't do anything if byte is not 0
     jne     char_loop
 
-    ; Bracket counter
-    mov     r13, 1
-march_to_matching_close:
-    inc     rbx
-    cmp     byte [rbx], '['
-    jne     no_opening_bracket
-    inc     r13
-    jmp     march_to_matching_close
-no_opening_bracket:
-    cmp     byte [rbx], ']'
-    jne     march_to_matching_close
-    dec     r13 ; Seen closing bracket, reduce bracket counter
+    mov     rbx, 1                  ; Bracket counter
+    .march_to_matching:
+        inc     r9
+        cmp     byte [r9], '['
+        jne     .no_opening_bracket
+        inc     rbx                 ; Seen opening bracket, increment bracket counter
+        jmp     .march_to_matching
+    .no_opening_bracket:
+        cmp     byte [r9], ']'
+        jne     .march_to_matching
+        dec     rbx                 ; Seen closing bracket, reduce bracket counter
 
-    ; Only matching if r13 is 0
-    cmp     r13, 0
-    jne     march_to_matching_close
-    jmp     char_loop
+        test    rbx, rbx
+        jne     .march_to_matching  ; Bracket counter not 0? Continue to find matching bracket
+        jmp     char_loop           ; Bracket counter 0, we're done here
 
-if_nonzero_go_back:
-    ; Don't do anything if data pointer is 0
-    cmp     byte [r12], 0
+
+op_jmp_bw:
+    cmp     byte [r10], 0           ; Don't do anything if byte is 0
     je      char_loop
 
-    ; Bracket counter
-    mov     r13, 1
-march_to_matching_open:
-    dec     rbx
-    cmp     byte [rbx], ']'
-    jne     no_closing_bracket
-    inc     r13
-    jmp     march_to_matching_open
-no_closing_bracket:
-    cmp     byte [rbx], '['
-    jne     march_to_matching_open
-    dec     r13 ; Seen open bracket, reduce bracket counter
+    mov     rbx, 1                  ; Bracket counter
+    .march_to_matching:
+        dec     r9
+        cmp     byte [r9], ']'
+        jne     .no_closing_bracket
+        inc     rbx                 ; Seen closing bracket, increment bracket counter
+        jmp     .march_to_matching
+    .no_closing_bracket:
+        cmp     byte [r9], '['
+        jne     .march_to_matching
+        dec     rbx                 ; Seen opening bracket, reduce bracket counter
 
-    ; Only matching if r13 is 0
-    cmp     r13, 0
-    jne     march_to_matching_open
-    jmp     char_loop
+        test    rbx, rbx
+        jne     .march_to_matching  ; Bracket counter not 0? Continue to find matching bracket
+        jmp     char_loop           ; Bracket counter 0, we're done here
 
 exit:
     mov     rax, 60     ; syscall exit
